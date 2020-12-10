07b4e67d00a40c48e06ded87cb926ccdbb0aa342
kenzan8000 <kenzan8000@gmail.com>
2020-12-10 21:50:20 +0900

    add Mapbox Map SwiftUI

---
+++ bathymetry/Map/MapView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+import Mapbox
+
+// MARK: - MapView
+struct MapView: UIViewRepresentable {
+    
+    // MARK: - property
+    
+    private let mapView: MGLMapView = MGLMapView(frame: .zero, styleURL: MGLStyle.streetsStyleURL)
+    
+    // MARK: - UIViewRepresentable
+    
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MGLMapView {
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MGLMapView, context: UIViewRepresentableContext<MapView>) {
+    }
+    
+    func makeCoordinator() -> MapView.Coordinator {
+        Coordinator(self)
+    }
+    
+    // MARK: - MGLMapViewDelegate
+    
+    final class Coordinator: NSObject, MGLMapViewDelegate {
+        var control: MapView
+        
+        init(_ control: MapView) {
+            self.control = control
+        }
+    }
+    
+}
+
+
