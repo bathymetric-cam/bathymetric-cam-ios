d48c11595865203ae8f635457c6eec11a351eba0
kenzan8000 <kenzan8000@gmail.com>
2020-12-19 00:44:50 +0900

    update mapview layout (dark mode, border)

--- bathymetry/ContentView.swift
+++ bathymetry/ContentView.swift
@@ -5,6 +5,8 @@ struct ContentView: View {
 
     // MARK: - property
     
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
             ZStack {
                 ARView()
@@ -16,6 +18,11 @@ struct ContentView: View {
                         )
                         .cornerRadius(metrics.size.width / 2.0)
                         .offset(y: metrics.size.height - metrics.size.width / 2.0)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: metrics.size.width / 2.0)
+                                .stroke(Color.gray, lineWidth: 4)
+                                .offset(y: metrics.size.height - metrics.size.width / 2.0)
+                        )
                 }
             }
             .edgesIgnoringSafeArea(.all)
--- bathymetry/Map/MapView.swift
+++ bathymetry/Map/MapView.swift
@@ -1,27 +1,59 @@
 import Mapbox
 import SwiftUI
 
+// MARK: - UIMapView
+final class UIMapView: MGLMapView {
+    
+    // MARK: - property
+    
+    // MARK: - initialization
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override init(frame: CGRect, styleURL: URL?) {
+        MGLAccountManager.accessToken = "pk.eyJ1Ijoia2VuemFuODAwMCIsImEiOiJja2Rva2R5aHgxdDYxMndxM2p4d20wMDRwIn0.waANNEC_jYG1_Hk5OExA3A"
+        super.init(frame: frame, styleURL: styleURL)
+        self.styleURL = traitCollection.userInterfaceStyle == .dark ? MGLStyle.darkStyleURL : MGLStyle.lightStyleURL
+        showsUserLocation = true
+        zoomLevel = 15
+        isZoomEnabled = false
+        isScrollEnabled = false
+        isRotateEnabled = false
+        isPitchEnabled = false
+        compassView.isHidden = true
+    }
+    
+    // MARK: - destruction
+    
+    deinit {
+    }
+    
+    // MARK: - life cycle
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        styleURL = traitCollection.userInterfaceStyle == .dark ? MGLStyle.darkStyleURL : MGLStyle.lightStyleURL
+    }
+    
+}
+
 // MARK: - MapView
 struct MapView: UIViewRepresentable {
     
     // MARK: - property
     
-    private let mapView: MGLMapView = MGLMapView(frame: .zero, styleURL: MGLStyle.streetsStyleURL)
+    private let mapView: UIMapView = UIMapView(frame: .zero, styleURL: MGLStyle.streetsStyleURL)
     
     // MARK: - UIViewRepresentable
     
-    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MGLMapView {
-        MGLAccountManager.accessToken = "pk.eyJ1Ijoia2VuemFuODAwMCIsImEiOiJja2Rva2R5aHgxdDYxMndxM2p4d20wMDRwIn0.waANNEC_jYG1_Hk5OExA3A"
-        mapView.showsUserLocation = true
-        mapView.zoomLevel = 15
-        mapView.compassView.isHidden = true
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> UIMapView {
         mapView.delegate = context.coordinator
-        mapView.tintColor = .systemBlue
-        mapView.isUserInteractionEnabled = false
         return mapView
     }
     
-    func updateUIView(_ uiView: MGLMapView, context: UIViewRepresentableContext<MapView>) {
+    func updateUIView(_ uiView: UIMapView, context: UIViewRepresentableContext<MapView>) {
     }
     
     static func dismantleUIView(_ uiView: MapView.UIViewType, coordinator: MapView.Coordinator) {
