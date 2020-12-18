b45dea51a85d5377fa47850be69b1667a07000a6
kenzan8000 <kenzan8000@gmail.com>
2020-12-19 00:20:35 +0900

    add heading to mapview

--- bathymetry/Map/MapView.swift
+++ bathymetry/Map/MapView.swift
@@ -12,7 +12,12 @@ struct MapView: UIViewRepresentable {
     
     func makeUIView(context: UIViewRepresentableContext<MapView>) -> MGLMapView {
         MGLAccountManager.accessToken = "pk.eyJ1Ijoia2VuemFuODAwMCIsImEiOiJja2Rva2R5aHgxdDYxMndxM2p4d20wMDRwIn0.waANNEC_jYG1_Hk5OExA3A"
+        mapView.showsUserLocation = true
+        mapView.zoomLevel = 15
+        mapView.compassView.isHidden = true
         mapView.delegate = context.coordinator
+        mapView.tintColor = .systemBlue
+        mapView.isUserInteractionEnabled = false
         return mapView
     }
     
@@ -26,13 +31,28 @@ struct MapView: UIViewRepresentable {
         Coordinator(self)
     }
     
-    // MARK: - Coordinator, MGLMapViewDelegate
+    // MARK: - Coordinator
     
     final class Coordinator: NSObject, MGLMapViewDelegate {
         var control: MapView
         
         init(_ control: MapView) {
             self.control = control
+        }
+        
+        // MARK: - MGLMapViewDelegate
+        
+        func mapView(_ mapView: MGLMapView, didUpdate userLocation: MGLUserLocation?) {
+            if control.mapView.userTrackingMode == .followWithHeading {
+                return
+            }
+            if let coordinate = userLocation?.coordinate {
+                control.mapView.setCenter(coordinate, animated: false)
+            }
+            if let heading = userLocation?.heading?.trueHeading {
+                control.mapView.setDirection(heading, animated: false)
+            }
+            control.mapView.userTrackingMode = .followWithHeading
         }
     }
     
