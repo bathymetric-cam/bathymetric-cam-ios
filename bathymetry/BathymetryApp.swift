873d55c765eebdadbcd3b2b188d9dcf2a135d2fd
kenzan8000 <kenzan8000@gmail.com>
2021-01-03 20:30:05 +0900

    add logger to the project

--- bathymetry/BathymetryApp.swift
+++ bathymetry/BathymetryApp.swift
@@ -1,5 +1,8 @@
 import ComposableArchitecture
+import os
 import SwiftUI
+
+let log = Logger(subsystem: "\(Bundle.main.bundleIdentifier ?? "").logger", category: "main")
 
 // MARK: - BathymetryApp
 @main
