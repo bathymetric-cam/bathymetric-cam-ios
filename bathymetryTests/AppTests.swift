6b401bea26374f9624d915d8ca689f52df7b32b6
kenzan8000 <kenzan8000@gmail.com>
2021-01-20 22:12:45 +0900

    add unit test when loadBathymetriesFailure

--- bathymetryTests/AppTests.swift
+++ bathymetryTests/AppTests.swift
@@ -22,7 +22,7 @@ class AppTests: XCTestCase {
     
     // MARK: - test
     
-    func testAppStore_whenInitialState_loadBathymetries() throws {
+    func testAppStore_whenInitialState_loadBathymetriesSuccess() throws {
         let sut = TestStore(
           initialState: .init(),
           reducer: appReducer,
@@ -33,20 +33,38 @@ class AppTests: XCTestCase {
         )
         sut.assert(
             .environment {
-                $0.bathymetryClient.loadBathymetries = { _ in Effect(value: mockBathymetries) }
+                $0.bathymetryClient.loadBathymetries = { _ in Effect(value: mockBathymetryTiles) }
             },
             .send(.loadBathymetries(mockRegion)),
             .do { self.scheduler.advance(by: 0.3) },
-            .receive(.bathymetriesResult(.success(mockBathymetries))) { _ in
-            }
+            .receive(.bathymetriesResult(.success(mockBathymetryTiles)))
         )
     }
-
+    
+    func testAppStore_whenInitialState_loadBathymetriesFailure() throws {
+        let sut = TestStore(
+          initialState: .init(),
+          reducer: appReducer,
+          environment: AppEnvironment(
+            mainQueue: scheduler.eraseToAnyScheduler(),
+            bathymetryClient: .mock()
+          )
+        )
+        sut.assert(
+            .environment {
+                $0.bathymetryClient.loadBathymetries = { _ in Effect(error: mockFailure) }
+            },
+            .send(.loadBathymetries(mockRegion)),
+            .do { self.scheduler.advance(by: 0.3) },
+            .receive(.bathymetriesResult(.failure(mockFailure)))
+        )
+    }
 }
 
 // MARK: - mock
-private let mockBathymetries = [Bathymetry(x: 57483, y: 25954, zoom: 16)]
+private let mockBathymetryTiles = [BathymetryTile(x: 57483, y: 25954, zoom: 16, features: [])]
 private let mockRegion = Region(
     swTile: RegionTile(x: 57483, y: 25954, zoom: 16),
     neTile: RegionTile(x: 57483, y: 25954, zoom: 16)
 )
+private let mockFailure = BathymetryClient.Failure()
