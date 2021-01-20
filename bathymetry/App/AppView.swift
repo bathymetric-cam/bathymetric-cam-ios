5ad14c5e46fa0538c5a1ab020be793197df24b4f
kenzan8000 <kenzan8000@gmail.com>
2021-01-20 20:54:36 +0900

    update to use view modifiers instead of directly adding the settings to map view

--- bathymetry/App/AppView.swift
+++ bathymetry/App/AppView.swift
@@ -25,20 +25,35 @@ struct AppView: View {
                         .regionDidChange {
                             viewStore.send(.loadBathymetries($0))
                         }
-                        .frame(
-                            width: metrics.size.width,
-                            height: metrics.size.width
-                        )
-                        .cornerRadius(metrics.size.width / 2.0)
-                        .offset(y: metrics.size.height - metrics.size.width / 2.0)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: metrics.size.width / 2.0)
-                                .stroke(Color.gray, lineWidth: 4)
-                                .offset(y: metrics.size.height - metrics.size.width / 2.0)
-                        )
+                        .modifier(MapViewModifier(metrics: metrics))
                 }
             }
             .edgesIgnoringSafeArea(.all)
+        }
+    }
+    
+    // MARK: - MapViewModifier
+    
+    struct MapViewModifier: ViewModifier {
+        // MARK: - property
+        
+        let metrics: GeometryProxy
+        
+        // MARK: - public api
+        
+        func body(content: Content) -> some View {
+            content
+                .frame(
+                    width: metrics.size.width,
+                    height: metrics.size.width
+                )
+                .cornerRadius(metrics.size.width / 2.0)
+                .offset(y: metrics.size.height - metrics.size.width / 2.0)
+                .overlay(
+                    RoundedRectangle(cornerRadius: metrics.size.width / 2.0)
+                        .stroke(Color.gray, lineWidth: 4)
+                        .offset(y: metrics.size.height - metrics.size.width / 2.0)
+                )
         }
     }
     
