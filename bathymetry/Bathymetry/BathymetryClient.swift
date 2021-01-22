b60ff140abf60da111fb2a8fdba2fbf0647eed53
kenzan8000 <kenzan8000@gmail.com>
2021-01-22 17:00:52 +0900

    refactor BathymetryClient

--- bathymetry/Bathymetry/BathymetryClient.swift
+++ bathymetry/Bathymetry/BathymetryClient.swift
@@ -19,37 +19,11 @@ extension BathymetryClient {
 
     static let live = BathymetryClient { region in
         Future<[BathymetryTile], Failure> { promise in
-            guard let path = Bundle.main.path(forResource: "Contentful-Info", ofType: "plist"),
-               let plist = NSDictionary(contentsOfFile: path),
-               let spaceId = plist["spaceId"] as? String,
-               let accessToken = plist["accessToken"] as? String else {
+            guard let client = BathymetryContentfulClientFactory.create(type: .contentful) else {
                 promise(.failure(Failure()))
                 return
             }
-            let client = Client(spaceId: spaceId, accessToken: accessToken, contentTypeClasses: [Bathymetry.self])
-            let query = QueryOn<Bathymetry>
-                .where(field: .zoom, .equals("\(region.swTile.zoom)"))
-                .where(field: .x, .isGreaterThanOrEqualTo("\(region.swTile.x)"))
-                .where(field: .x, .isLessThanOrEqualTo("\(region.neTile.x)"))
-                .where(field: .y, .isGreaterThanOrEqualTo("\(region.neTile.y)"))
-                .where(field: .y, .isLessThanOrEqualTo("\(region.swTile.y)"))
-            client.fetchArray(of: Bathymetry.self, matching: query) {
-                guard case let .success(result) = $0 else {
-                    promise(.failure(Failure()))
-                    return
-                }
-                var bathymetryTiles: [BathymetryTile] = []
-                result.items.forEach {
-                    if let zoom = $0.zoom,
-                        let x = $0.x,
-                        let y = $0.y,
-                        let geoJSON = $0.geoJSON,
-                        case let .featureCollection(featureCollection) = geoJSON {
-                        bathymetryTiles.append(BathymetryTile(x: x, y: y, zoom: zoom, features: featureCollection.features))
-                    }
-                }
-                promise(.success(bathymetryTiles))
-            }
+            client.loadBathymetries(region: region, promise: promise)
         }
         .eraseToEffect()
     }
@@ -64,5 +38,69 @@ extension BathymetryClient {
         fatalError("Unmocked")
     }) -> Self {
         Self(loadBathymetries: loadBathymetries)
+    }
+}
+
+// MARK: - BathymetryInternalClientType
+internal enum BathymetryInternalClientType {
+    case contentful
+}
+
+// MARK: - BathymetryInternalClientFactory
+internal protocol BathymetryInternalClientFactory {
+    static func create(type: BathymetryInternalClientType) -> BathymetryContentfulClient?
+}
+
+// MARK: - BathymetryContentfulClientFactory
+internal class BathymetryContentfulClientFactory: BathymetryInternalClientFactory {
+    /// Abstruct factory method
+    /// - Parameter type: type of client defined by BathymetryInternalClientType
+    /// - Returns: created factory
+    static func create(type: BathymetryInternalClientType) -> BathymetryContentfulClient? {
+        guard let path = Bundle.main.path(forResource: "Contentful-Info", ofType: "plist"),
+           let plist = NSDictionary(contentsOfFile: path),
+           let spaceId = plist["spaceId"] as? String,
+           let accessToken = plist["accessToken"] as? String else {
+            return nil
+        }
+        return BathymetryContentfulClient(spaceId: spaceId, accessToken: accessToken, contentTypeClasses: [Bathymetry.self])
+    }
+}
+
+// MARK: - BathymetryInternalClient
+protocol BathymetryInternalClient {
+    /// load Bathymetry array by the current map region
+    /// - Parameters:
+    ///   - region: Region
+    ///   - promise: Result of the load
+    func loadBathymetries(region: Region, promise: @escaping (Result<[BathymetryTile], BathymetryClient.Failure>) -> Void)
+}
+
+// MARK: - BathymetryContentfulClient
+internal class BathymetryContentfulClient: Client, BathymetryInternalClient {
+    func loadBathymetries(region: Region, promise: @escaping (Result<[BathymetryTile], BathymetryClient.Failure>) -> Void) {
+        let query = QueryOn<Bathymetry>
+            .where(field: .zoom, .equals("\(region.swTile.zoom)"))
+            .where(field: .x, .isGreaterThanOrEqualTo("\(region.swTile.x)"))
+            .where(field: .x, .isLessThanOrEqualTo("\(region.neTile.x)"))
+            .where(field: .y, .isGreaterThanOrEqualTo("\(region.neTile.y)"))
+            .where(field: .y, .isLessThanOrEqualTo("\(region.swTile.y)"))
+        fetchArray(of: Bathymetry.self, matching: query) {
+            guard case let .success(result) = $0 else {
+                promise(.failure(BathymetryClient.Failure()))
+                return
+            }
+            var bathymetryTiles: [BathymetryTile] = []
+            result.items.forEach {
+                if let zoom = $0.zoom,
+                    let x = $0.x,
+                    let y = $0.y,
+                    let geoJSON = $0.geoJSON,
+                    case let .featureCollection(featureCollection) = geoJSON {
+                    bathymetryTiles.append(BathymetryTile(x: x, y: y, zoom: zoom, features: featureCollection.features))
+                }
+            }
+            promise(.success(bathymetryTiles))
+        }
     }
 }
