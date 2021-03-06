ba343b082cecc8ec9636c688ea4eceed801cbc09
kenzan8000 <kenzan8000@gmail.com>
2021-03-06 23:14:29 +0900

    rename coredata manager -> storage provider and update the di

--- bathymetry/CoreData/StorageProvider.swift
+++ bathymetry/CoreData/StorageProvider.swift
@@ -1,7 +1,7 @@
 import CoreData
 
-// MARK: - CoreDataManager
-class CoreDataManager {
+// MARK: - StorageProvider
+class StorageProvider {
   // MARK: static constant
   // static let sharedInstance = CoreDataManager()
 
@@ -12,8 +12,12 @@ class CoreDataManager {
   }
   
   // MARK: initialization
-  init() {
-    persistentContainer = NSPersistentContainer(name: "CoreData")
+  init(name: String, group: String) {
+    persistentContainer = NSPersistentContainer(name: name)
+    guard let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: group) else {
+      fatalError("Could not find coredata group url.")
+    }
+    persistentContainer.persistentStoreDescriptions.first?.url = url.appendingPathComponent("\(name).sqlite")
     persistentContainer.loadPersistentStores { _, error in
       if let error = error {
         fatalError("Core Data store failed to load with error: \(error)")
